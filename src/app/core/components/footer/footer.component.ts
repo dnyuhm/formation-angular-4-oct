0aa84b4a729204bd5fb7dd9f2e0a81fb374e3c25
dylanmeuriot <dylanmeuriot@gmail.com>
2021-10-06 11:49:22 +0200

    Commit 24 : Exercice “version” sur les services & observables

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { VersionService } from '../../services/version.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public v!: number;
+  constructor(private versionService: VersionService) {
+    this.versionService.numVersion.subscribe((number) => (this.v = number));
   }
 
+  ngOnInit(): void {}
 }
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VersionService } from '../../services/version.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  public v!: number;
+  constructor(private versionService: VersionService) {
+    this.versionService.numVersion.subscribe((number) => (this.v = number));
+  }
 
   ngOnInit(): void {}
 }
